abfd398e423cc47f980c9cd627794ae231472c27
FabioRNobrega <fabio.r.nobrega@gmail.com>
2023-09-14 09:17:59 -0300

    fix(migration): learnings

--- pg-core/db/migrate/20230714115342_create_learning.rb
+++ pg-core/db/migrate/20230714115342_create_learning.rb
@@ -1,13 +1,15 @@
 class CreateLearning < ActiveRecord::Migration[6.1]
   def change
-    create_table :learnings do |t|
-      t.string :title, null: false
-      t.text :description, null: false
-      t.string :author, null: false
-      t.string :reference_link
-      t.string :image_link
+    unless table_exists?(:learnings)
+      create_table :learnings do |t|
+        t.string :title, null: false
+        t.text :description, null: false
+        t.string :author, null: false
+        t.string :reference_link
+        t.string :image_link
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
