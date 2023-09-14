fa01e5df756b728836d67d5094cd75f8df7550aa
FabioRNobrega <fabio.r.nobrega@gmail.com>
2023-09-14 09:15:12 -0300

    fix(migration)

--- pg-core/db/migrate/20230507144138_create_goals_list.rb
+++ pg-core/db/migrate/20230507144138_create_goals_list.rb
@@ -1,12 +1,14 @@
 class CreateGoalsList < ActiveRecord::Migration[6.1]
   def change
-    create_table :goals_lists do |t|
-      t.string :title, null: false
-      t.string :description, null: false
-      t.boolean :is_public, :default => false
-      t.belongs_to :user
+    unless table_exists?(:goals_lists)
+      create_table :goals_lists do |t|
+        t.string :title, null: false
+        t.string :description, null: false
+        t.boolean :is_public, default: false
+        t.belongs_to :user
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
