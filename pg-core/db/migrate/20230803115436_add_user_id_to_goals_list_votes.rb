bf4de6240b1b0f2eaf9a6717f3903f2616005818
FabioRNobrega <fabio.r.nobrega@gmail.com>
2023-09-14 09:26:47 -0300

    fix(migration): user_id relation on lgoals_list_votes

--- pg-core/db/migrate/20230803115436_add_user_id_to_goals_list_votes.rb
+++ pg-core/db/migrate/20230803115436_add_user_id_to_goals_list_votes.rb
@@ -1,5 +1,7 @@
 class AddUserIdToGoalsListVotes < ActiveRecord::Migration[6.1]
   def change
-    add_reference :goals_list_votes, :user, null: false, foreign_key: true
+    unless column_exists?(:goals_list_votes, :user_id)
+      add_reference :goals_list_votes, :user, null: false, foreign_key: true
+    end
   end
 end
