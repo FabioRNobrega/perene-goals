f517406b8718da277710ea5074d804fcf8d3d2df
FabioRNobrega <fabio.r.nobrega@gmail.com>
2023-09-14 09:21:41 -0300

    fix(migration): goals_list and user_votes

--- pg-core/db/migrate/20230802113753_create_goals_list_votes.rb
+++ pg-core/db/migrate/20230802113753_create_goals_list_votes.rb
@@ -1,11 +1,13 @@
 class CreateGoalsListVotes < ActiveRecord::Migration[6.1]
   def change
-    create_table :goals_list_votes do |t|
-      t.references :goals_list, null: false, foreign_key: true
-      t.integer :votes_up
-      t.integer :votes_down
+    unless table_exists?
+      create_table :goals_list_votes do |t|
+        t.references :goals_list, null: false, foreign_key: true
+        t.integer :votes_up
+        t.integer :votes_down
 
-      t.timestamps
+        t.timestamps
+      end
     end
   end
 end
--- pg-core/db/migrate/20230802120156_create_user_votes.rb
+++ pg-core/db/migrate/20230802120156_create_user_votes.rb
@@ -1,12 +1,14 @@
 class CreateUserVotes < ActiveRecord::Migration[6.1]
   def change
-    create_table :user_votes do |t|
-      t.references :user, null: false, foreign_key: true
-      t.references :goals_list, null: false, foreign_key: true
-      t.integer :votes_up, default: 0
-      t.integer :votes_down, default: 0
+    unless table_exists?(:user_votes)
+      create_table :user_votes do |t|
+        t.references :user, null: false, foreign_key: true
+        t.references :goals_list, null: false, foreign_key: true
+        t.integer :votes_up, default: 0
+        t.integer :votes_down, default: 0
 
-      t.timestamps
+        t.timestamps
+      end
     end
 
     add_index :user_votes, [:user_id, :goals_list_id], unique: true
